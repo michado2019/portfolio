1c7215e5595755f2ffd4a72c9b1b3e24bbad65c0
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-02-07 00:54:24 +0100

    feat: Navbar.tsx created and rendered in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
   return (
     <>
-      <div className="text-[#de5748]">Hello</div>
+      <div className="text-[#de5748]">
+        <Navbar />
+      </div>
     </>
   );
 }
