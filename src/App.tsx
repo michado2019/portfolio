0117eb23318b1302b133e749a4ba71dd85082889
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-02-07 01:43:35 +0100

    feat: Navbar.tsx content added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/navbar/Navbar";
 import AppRouter from "./components/routes/AppRouter";
 
 function App() {
-
   return (
     <>
       <div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,37 +1,39 @@
-import { Link } from "react-router-dom"
-import logo from "./assets/_logo.png"
+import { Link } from "react-router-dom";
+import logo from "./assets/_logo.png";
+import { Email, GitHub, LinkedIn, Twitter } from "@mui/icons-material";
 const Navbar = () => {
   return (
     <div className="navbar-wrapper">
-<div className="navbar-contents flex">
-<div className="navbar-content1 flex mr-auto">
-    <img src={logo} alt="img" className="" />
-        <h2>Michado</h2>
-</div>
-<div className="navbar-content2 flex">
-<Link to="/">Home</Link>
-<Link to="/blogs">Blogs</Link>
-</div>
-<div className="navbar-content3 flex">
-<div>
-
-</div>
-<div>
-    
-</div>
-<div>
-    
-</div>
-<div>
-    
-</div>
-</div>
-</div>
-instagram
-Discord
-Github
+      <div className="navbar-contents flex">
+        <div className="navbar-content1 flex mr-auto">
+          <img src={logo} alt="img" className="" />
+          <h2>Michado</h2>
+        </div>
+        <div className="navbar-content2 flex mr-auto">
+          <Link to="/">Home</Link>
+          <Link to="/blogs">Blogs</Link>
+        </div>
+        <div className="navbar-content3 flex">
+          <div className="flex items-center">
+            <GitHub />
+            <h2>Github</h2>
+          </div>
+          <div className="flex items-center">
+            <LinkedIn />
+            <h2>LinkedIn</h2>
+          </div>
+          <div className="flex items-center">
+            <Twitter />
+            <h2>Twitter</h2>
+          </div>
+          <div className="flex items-center">
+            <Email />
+            <h2>Email</h2>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
