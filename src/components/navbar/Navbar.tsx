cc5483415aef84404b10a912fdc52a7c8b734911
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-02-07 03:35:47 +0100

    style: Navbar.tsx styled

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,34 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "./assets/_logo.png";
 import { Email, GitHub, LinkedIn, Twitter } from "@mui/icons-material";
+import "./Navbar.css";
+type Path = "/" | "/blogs";
 const Navbar = () => {
+  const location = useLocation();
+
+  // Function to determine if a link is active
+  const isActiveLink = (path: Path) => {
+    return location.pathname === path;
+  };
+
   return (
-    <div className="navbar-wrapper">
-      <div className="navbar-contents flex">
-        <div className="navbar-content1 flex mr-auto">
-          <img src={logo} alt="img" className="" />
-          <h2>Michado</h2>
-        </div>
-        <div className="navbar-content2 flex mr-auto">
-          <Link to="/">Home</Link>
-          <Link to="/blogs">Blogs</Link>
+    <div className="navbar-wrapper bg-[#2d2e2e] py-7 px-12">
+      <div className="navbar-contents flex items-center justify-between">
+        <Link to="/" className="navbar-content1 flex">
+          <img src={logo} alt="img" className="mx-[2px] w-12 h-6" />
+          <h2 className="text-[#fff]">Michado</h2>
+        </Link>
+        <div className="navbar-content2 flex">
+          <Link
+            to="/"
+            className={isActiveLink("/") ? "active-link" : "inactive-link"}
+          >
+            Home
+          </Link>
+          <Link
+            to="/blogs"
+            className={isActiveLink("/blogs") ? "active-link" : "inactive-link"}
+          >
+            Blogs
+          </Link>
         </div>
         <div className="navbar-content3 flex">
-          <div className="flex items-center">
-            <GitHub />
-            <h2>Github</h2>
+          <div className="flex items-center mx-2 cursor-pointer">
+            <GitHub className="text-[#39ccca] w-2 h-2 mui" />
+            <h2 className="text-[#fff] mx-[2px] text-[14px]">Github</h2>
           </div>
-          <div className="flex items-center">
-            <LinkedIn />
-            <h2>LinkedIn</h2>
+          <div className="flex items-center mx-2 cursor-pointer">
+            <LinkedIn className="text-[#39ccca] w-2 h-2 mui" />
+            <h2 className="text-[#fff] mx-[2px] text-[14px]">LinkedIn</h2>
           </div>
-          <div className="flex items-center">
-            <Twitter />
-            <h2>Twitter</h2>
+          <div className="flex items-center mx-2 cursor-pointer">
+            <Twitter className="text-[#39ccca] w-2 h-2 mui" />
+            <h2 className="text-[#fff] mx-[2px] text-[14px]">Twitter</h2>
           </div>
-          <div className="flex items-center">
-            <Email />
-            <h2>Email</h2>
+          <div className="flex items-center mx-2 cursor-pointer">
+            <Email className="text-[#39ccca] w-2 h-2 mui" />
+            <h2 className="text-[#fff] mx-[2px] text-[14px]">Email</h2>
           </div>
         </div>
       </div>
