2ba63004bb77960041d04797c1e5a52441d4ff20
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-02-14 13:54:29 +0100

    feat: About.tsx created and rendered on homepage

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,9 +1,11 @@
+import About from "./about/About"
 import Hero from "./hero/Hero"
 
 const Home = () => {
   return (
     <div className="mt-[80px]">
         <Hero />
+        <About />
     </div>
   )
 }
