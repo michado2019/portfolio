45a1de6875acb38d0ed21638c16049c79785ef53
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-02-22 03:01:41 +0100

    feat: Skills.tsx created and rendered in Home.tsx

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import About from "./about/About"
 import Hero from "./hero/Hero"
+import Skills from "./skills/Skills"
 
 const Home = () => {
   return (
     <div className="mt-[80px]">
         <Hero />
         <About />
+        <Skills />
     </div>
   )
 }
---
+++ src/components/pages/home/skills/Skills.tsx
@@ -0,0 +1,8 @@
+
+const Skills = () => {
+  return (
+    <div className='bg-[#292F36]'>Skills</div>
+  )
+}
+
+export default Skills
